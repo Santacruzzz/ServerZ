d08b4436d7a3a81e16dae5b286d6366fb55bcfa2
Kasia <katarzynadudziak94@gmail.com>
2024-02-15 12:45:04 +0100

    Changed active_server view.

--- app/src/main/java/pl/tmkd/serverz/SecondActivity.java
+++ app/src/main/java/pl/tmkd/serverz/SecondActivity.java
@@ -2,12 +2,14 @@ package pl.tmkd.serverz;
 
 import static android.widget.Toast.LENGTH_SHORT;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,6 +29,7 @@ import pl.tmkd.serverz.sq.ServerListener;
 public class SecondActivity extends Activity implements ServerListener, Serializable {
     private View view;
     private TextView textView;
+    private ProgressBar progressBar;
     Server server;
     ArrayAdapter<String> itemsAdapter;
 
@@ -35,6 +38,7 @@ public class SecondActivity extends Activity implements ServerListener, Serializ
         super.onCreate(savedInstanceState);
         View view = getLayoutInflater().inflate(R.layout.active_server, null);
         setContentView(view);
+        progressBar = findViewById(R.id.progressBar);
         Intent intent = getIntent();
         String ip = intent.getStringExtra("ip");
         int port = intent.getIntExtra("port", 0);
@@ -52,18 +56,23 @@ public class SecondActivity extends Activity implements ServerListener, Serializ
         listView.setAdapter(itemsAdapter);
     }
 
+    @SuppressLint("SetTextI18n")
     public void updateServerInfo() {
         TextView name = findViewById(R.id.serverName);
         TextView amountOfPlayers = findViewById(R.id.amountOfPlayers);
         TextView serverTime = findViewById(R.id.serverTime);
         TextView dayDuration = findViewById(R.id.dayDuration);
         TextView nightDuration = findViewById(R.id.nightDuration);
+        TextView durationTillSunriseOrSunset = findViewById(R.id.durationTillSunriseOrSunset);
+        progressBar.findViewById(R.id.progressBar);
 
         name.setText(server.getName());
-        amountOfPlayers.setText(String.valueOf(server.getPlayersNum()));
+        amountOfPlayers.setText((server.getPlayersNum()) + "/" + (server.getMaxPlayers()));
         serverTime.setText(server.getServerTime());
         dayDuration.setText(server.getDayDuration());
         nightDuration.setText(server.getNightDuration());
+        durationTillSunriseOrSunset.setText(server.getDurationTillSunriseOrSunset());
+        progressBar.setProgress(server.getDayOrNightProgress());
     }
 
     @Override
